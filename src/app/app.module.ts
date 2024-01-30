4782da361e48300f40ed5e7b4f38acbd1069a4e0
Valerii <valeriy.stronskiy@gmail.com>
2024-01-30 20:00:19 +0200

    Module angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterOutlet } from "@angular/router";
+
+@NgModule
+(
+    {
+        imports: [BrowserModule, RouterOutlet],
+        declarations: [],
+        bootstrap: []
+    }
+)
+
+export class AppModule
+{
+
+}
--- src/app/shop/product-list/product-list.component.ts
+++ src/app/shop/product-list/product-list.component.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Product } from '../Product';
 import { catchError, map } from 'rxjs';
+import { ProductService } from '../product.service';
 
 @Component
 ({
@@ -17,13 +18,14 @@ export class ProductListComponent
 {
   products: Product[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private productService: ProductService) {}
 
   ngOnInit()
   {
     // this.http.get('https://fakestoreapi.com/products').subscribe( (data) => this.products = <Product[]>data );
-    this.http.get<Product[]>('https://fakestoreapi.com/products')
-    .pipe(map((data: any) => data.map((p:any) => {return {...p, price: p.price *= 40}})))
+    // this.http.get<Product[]>('https://fakestoreapi.com/products')
+    this.productService.getProducts()
+    .pipe(map((data: any) => data.map((p: any) => {return {...p, price: p.price *= 40}})))
     .subscribe((data) => this.products = data);
   }
 }
